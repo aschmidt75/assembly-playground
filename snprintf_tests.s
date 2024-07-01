e5d852829d8b0b0eb88821cdcec757fb43e939d6
Andreas Schmidt <aschmidt75@web.de>
2024-07-01 21:28:55 +0200

    add: %c test

--- snprintf_tests.s
+++ snprintf_tests.s
@@ -24,9 +24,47 @@ jal     _snprintf
 
 add     a1, zero, a0        # a1 is =buf, a2 is the original compare str
 jal     _streq              # compare
-la      ra, L_snprintf_test_99   # use ra in this routine to skip to end
+la      ra, L_snprintf_test_2   # use ra in this routine to skip to next
 beqz    a0, L_snprintf_P    # if ==0, strings are equals, passed
 j       L_snprintf_F        # fail otherwise, will return here
+
+
+# test: pattern with escape fmt parts must yield correct string
+L_snprintf_test_2:
+la      a0, snprintf_t_buf
+li      a1, 100
+la      a2, snprintf_t_pat_esc
+jal     _snprintf
+
+add     a1, zero, a0        
+la      a2, snprintf_t_pat_esc_r
+jal     _streq              
+la      ra, L_snprintf_test_3  
+beqz    a0, L_snprintf_P    
+j       L_snprintf_F        
+
+# test: pattern with char fmt parts must yield correct string
+L_snprintf_test_3:
+addi    sp, sp, -8
+li      a1, 0x30          # 0x30 = "0", put on stack
+sb      a1, 0(sp)
+li      a1, 0x31          # "1"
+sb      a1, 1(sp)
+li      a1, 0x32          # "2"
+sb      a1, 2(sp)
+la      a0, snprintf_t_buf
+li      a1, 100
+la      a2, snprintf_t_pat_char
+jal     _snprintf
+addi    sp, sp, 8
+
+add     a1, zero, a0        
+la      a2, snprintf_t_pat_char_r
+jal     _streq              
+la      ra, L_snprintf_test_99   
+beqz    a0, L_snprintf_P    
+j       L_snprintf_F        
+
 
 
 # test: empty pattern must yield empty string
@@ -82,3 +120,11 @@ snprintf_t_empty:
 .byte 0
 snprintf_t_nopat:
 .asciiz "SimpleTestWithoutFmt"
+snprintf_t_pat_esc:
+.asciiz "%%Simple%%Escape%%"
+snprintf_t_pat_esc_r:
+.asciiz "%Simple%Escape%"
+snprintf_t_pat_char:
+.asciiz "%cSimple%cPat%c"
+snprintf_t_pat_char_r:
+.asciiz "0Simple1Pat2"
